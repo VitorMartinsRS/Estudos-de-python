f979482a443bb863828e77f0bdda8500b6f36f33
vitormartins <vitorrsilveriomartins@gmail.com>
2025-09-16 16:21:27 -0300

    aula

--- code19.py
+++ code19.py
@@ -1,14 +1,14 @@
-# txt= "Python"
+txt= "Python"
 
-# for letra in txt:
-#     print(letra)
-# txt= "Python"
-# new_txt= ''
-# for letra in txt:
-#     new_txt += f'*{letra}'
-# print(new_txt)
-bit = range( 10)
-for valor in bit:
+for letra in txt:
+    print(letra)
+txt= "Python"
+new_txt= ''
+for letra in txt:
+    new_txt += f'*{letra}'
+print(new_txt)
+# bit = range( 10)
+# for valor in bit:
 
-    print(valor )
+#     print(valor )
 
--- code21.py
+++ code21.py
@@ -1,13 +1,47 @@
+p_secreta = 'ana'
+letras_acertadas = []
+tentativas = 0
+LIMITE_TENTATIVAS = 5
 
-for i in range(5):
-    print('-'*30)
-    print(f'Você tem {i} tentativas de 5')
-    print('-'*30)
-    letra = ''
-    try:
-        letra=input("Informe a apenas uma letra para adivinhar a palavra secreta secreta: ") 
-        letra_new=str(letra)
+print('-'*40)
+print('Bem-vindo ao Jogo da Forca!')
+print('-'*40)
+
+while tentativas < LIMITE_TENTATIVAS:
+    print(f'Você tem {LIMITE_TENTATIVAS - tentativas} tentativas restantes.')
+    
+    
+    palavra_exibicao = ''
+    for letra in p_secreta:
+        if letra in letras_acertadas:
+            palavra_exibicao += letra
+        else:
+            palavra_exibicao += '_'
+            
+    print(f'Palavra: {palavra_exibicao}\n')
+
+    if palavra_exibicao == p_secreta:
+        print('Parabéns! Você adivinhou a palavra secreta!')
+        break
+
+    letra = input("Informe uma letra para adivinhar: ").lower().strip()
+    if letra ==' ':
+        if len(letra) != 1 or not letra.isalpha():
+            print('Entrada inválida. Por favor, digite apenas uma letra.\n')
+            continue
+        continue
+    if letra in letras_acertadas:
+        print('Você já tentou esta letra. Tente outra.\n')
+        continue
         
-    except:
-        print('Letra invalida')
+    letras_acertadas.append(letra)
+
+    if letra in p_secreta:
+        print('Certo! A letra está na palavra.\n')
+    else:
+        print('Ops! A letra não está na palavra.\n')
+        tentativas += 1
         
+if tentativas >= LIMITE_TENTATIVAS:
+    print('Suas tentativas acabaram! Você perdeu.')
+    print(f'A palavra secreta era: {p_secreta}')
