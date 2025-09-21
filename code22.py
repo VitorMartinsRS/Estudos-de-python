de8d647a7694f3bef3ac21ea3a6b921a423cab7c
Vitor Martins <vitorrsilveriomartins@gmail.com>
2025-09-20 22:32:51 -0300

    aula faculdade

---
+++ code22.py
@@ -0,0 +1,14 @@
+lista= [123, True, 'Vitor Martins', 1.2, []]
+print(lista[2])
+
+listass= [10,20,30,40]
+listass[2]=300
+print(listass[2])
+listass.append(50)
+print(listass)
+listass.pop(2)
+print(listass,listass.pop() )
+listass.clear()
+print(listass)
+listass.insert(0, 5)
+print(listass)
