d75756cb251f8aca797110a34871c6417d575484
VitorMartinsRS <vitorrsilveriomartins@gmail.com>
2025-09-26 02:56:26 -0300

    aula

---
+++ code32.py
@@ -0,0 +1,7 @@
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=}+ y={y}+ z={z}','|','x + y + z =',x+y+z)
+    else:
+        print(f'{x=}+ y={y}','|','x + y ',x+y)
+soma(1,2)
+soma(1,2,3)
